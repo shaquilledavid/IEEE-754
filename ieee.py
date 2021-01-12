17a908d823f210f104ee3062f9a59dcbb5ef8a32
Shaquille David <47598577+shaquilledavid@users.noreply.github.com>
2021-01-11 20:57:43 -0500

    added python file

---
+++ ieee.py
@@ -0,0 +1,17 @@
+"""
+2020 PyJaC coding competition.
+
+PyJaC is a brand-new coding competition brought to you by UTM’s Women in Science
+and Computing Club (WiSC), UTM Mathematical and Computational Sciences Society
+(MCSS), and UTM Physics Club (PC).
+
+Our prompt: creat an IEEE encoder/decoder
+
+An IEEE encoder/decoder should convert floating-point numbers to a decimal
+representation and back to floating-point, depending on the operation chosen.
+
+Project completed by: Shaquille David
+                      Junayeed Rahman
+                      Mohid Sharif
+"""
+import math
